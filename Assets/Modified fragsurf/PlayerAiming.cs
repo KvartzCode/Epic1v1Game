ce9f2910a450f0bbbc960e45afb1641b92999562
Proliix <lucas.andreasson@hotmail.com>
2023-05-16 10:44:05 +0200

    Update PlayerAiming.cs

--- Assets/Modified fragsurf/PlayerAiming.cs
+++ Assets/Modified fragsurf/PlayerAiming.cs
@@ -2,96 +2,105 @@
 
 public class PlayerAiming : MonoBehaviour
 {
-	[Header("References")]
-	public Transform bodyTransform;
+    [Header("References")]
+    public Transform bodyTransform;
 
-	[Header("Sensitivity")]
-	public float sensitivityMultiplier = 1f;
-	public float horizontalSensitivity = 1f;
-	public float verticalSensitivity   = 1f;
+    [Header("Sensitivity")]
+    public float sensitivityMultiplier = 1f;
+    public float horizontalSensitivity = 1f;
+    public float verticalSensitivity = 1f;
 
-	[Header("Restrictions")]
-	public float minYRotation = -90f;
-	public float maxYRotation = 90f;
+    [Header("Restrictions")]
+    public float minYRotation = -90f;
+    public float maxYRotation = 90f;
 
-	//The real rotation of the camera without recoil
-	private Vector3 realRotation;
+    //The real rotation of the camera without recoil
+    private Vector3 realRotation;
 
-	[Header("Aimpunch")]
-	[Tooltip("bigger number makes the response more damped, smaller is less damped, currently the system will overshoot, with larger damping values it won't")]
-	public float punchDamping = 9.0f;
+    [Header("Aimpunch")]
+    [Tooltip("bigger number makes the response more damped, smaller is less damped, currently the system will overshoot, with larger damping values it won't")]
+    public float punchDamping = 9.0f;
 
-	[Tooltip("bigger number increases the speed at which the view corrects")]
-	public float punchSpringConstant = 65.0f;
+    [Tooltip("bigger number increases the speed at which the view corrects")]
+    public float punchSpringConstant = 65.0f;
 
-	[HideInInspector]
-	public Vector2 punchAngle;
+    [HideInInspector]
+    public Vector2 punchAngle;
 
-	[HideInInspector]
-	public Vector2 punchAngleVel;
+    [HideInInspector]
+    public Vector2 punchAngleVel;
 
-	private void Start()
-	{
-		// Lock the mouse
-		Cursor.lockState = CursorLockMode.Locked;
-		Cursor.visible   = false;
-	}
+    private void Start()
+    {
+        LockMouse(true);
+    }
 
-	private void Update()
-	{
-		// Fix pausing
-		if (Mathf.Abs(Time.timeScale) <= 0)
-			return;
+    void LockMouse(bool isLocked)
+    {
+        // Lock the mouse
+        Cursor.lockState = isLocked ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !isLocked;
+    }
 
-		DecayPunchAngle();
+    private void Update()
+    {
 
-		// Input
-		float xMovement = Input.GetAxisRaw("Mouse X") * horizontalSensitivity * sensitivityMultiplier;
-		float yMovement = -Input.GetAxisRaw("Mouse Y") * verticalSensitivity  * sensitivityMultiplier;
+        if (Input.GetKeyDown(KeyCode.Escape))
+            LockMouse(Cursor.visible);
 
-		// Calculate real rotation from input
-		realRotation   = new Vector3(Mathf.Clamp(realRotation.x + yMovement, minYRotation, maxYRotation), realRotation.y + xMovement, realRotation.z);
-		realRotation.z = Mathf.Lerp(realRotation.z, 0f, Time.deltaTime * 3f);
+        // Fix pausing
+        if (Mathf.Abs(Time.timeScale) <= 0)
+            return;
 
-		//Apply real rotation to body
-		bodyTransform.eulerAngles = Vector3.Scale(realRotation, new Vector3(0f, 1f, 0f));
+        DecayPunchAngle();
 
-		//Apply rotation and recoil
-		Vector3 cameraEulerPunchApplied = realRotation;
-		cameraEulerPunchApplied.x += punchAngle.x;
-		cameraEulerPunchApplied.y += punchAngle.y;
+        // Input
+        float xMovement = Input.GetAxisRaw("Mouse X") * horizontalSensitivity * sensitivityMultiplier;
+        float yMovement = -Input.GetAxisRaw("Mouse Y") * verticalSensitivity * sensitivityMultiplier;
 
-		transform.eulerAngles = cameraEulerPunchApplied;
-	}
+        // Calculate real rotation from input
+        realRotation = new Vector3(Mathf.Clamp(realRotation.x + yMovement, minYRotation, maxYRotation), realRotation.y + xMovement, realRotation.z);
+        realRotation.z = Mathf.Lerp(realRotation.z, 0f, Time.deltaTime * 3f);
 
-	public void ViewPunch(Vector2 punchAmount)
-	{
-		//Remove previous recoil
-		punchAngle = Vector2.zero;
+        //Apply real rotation to body
+        bodyTransform.eulerAngles = Vector3.Scale(realRotation, new Vector3(0f, 1f, 0f));
 
-		//Recoil go up
-		punchAngleVel -= punchAmount * 20;
-	}
+        //Apply rotation and recoil
+        Vector3 cameraEulerPunchApplied = realRotation;
+        cameraEulerPunchApplied.x += punchAngle.x;
+        cameraEulerPunchApplied.y += punchAngle.y;
 
-	private void DecayPunchAngle()
-	{
-		if (punchAngle.sqrMagnitude > 0.001 || punchAngleVel.sqrMagnitude > 0.001)
-		{
-			punchAngle += punchAngleVel * Time.deltaTime;
-			float damping = 1 - (punchDamping * Time.deltaTime);
+        transform.eulerAngles = cameraEulerPunchApplied;
+    }
 
-			if (damping < 0)
-				damping = 0;
+    public void ViewPunch(Vector2 punchAmount)
+    {
+        //Remove previous recoil
+        punchAngle = Vector2.zero;
 
-			punchAngleVel *= damping;
+        //Recoil go up
+        punchAngleVel -= punchAmount * 20;
+    }
 
-			float springForceMagnitude = punchSpringConstant * Time.deltaTime;
-			punchAngleVel -= punchAngle * springForceMagnitude;
-		}
-		else
-		{
-			punchAngle    = Vector2.zero;
-			punchAngleVel = Vector2.zero;
-		}
-	}
+    private void DecayPunchAngle()
+    {
+        if (punchAngle.sqrMagnitude > 0.001 || punchAngleVel.sqrMagnitude > 0.001)
+        {
+            punchAngle += punchAngleVel * Time.deltaTime;
+            float damping = 1 - (punchDamping * Time.deltaTime);
+
+            if (damping < 0)
+                damping = 0;
+
+            punchAngleVel *= damping;
+
+            float springForceMagnitude = punchSpringConstant * Time.deltaTime;
+            punchAngleVel -= punchAngle * springForceMagnitude;
+        }
+        else
+        {
+            punchAngle = Vector2.zero;
+            punchAngleVel = Vector2.zero;
+        }
+    }
 }
