ae7177c6fd54cede2efff99b88fe219114853a4c
KvartzCode <113012348+KvartzCode@users.noreply.github.com>
2023-05-25 13:17:41 +0200

    Created a new Toggle Component that actually works.

---
+++ Assets/Source UI/Editor/SrcToggleEditor.cs
@@ -0,0 +1,55 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityEditor.UI
+{
+    [CustomEditor(typeof(SrcToggle))]
+    [CanEditMultipleObjects]
+    public class SrcToggleEditor : ToggleEditor
+    {
+        SerializedProperty m_useCheckBox;
+        SerializedProperty m_targetImage;
+        SerializedProperty m_normalSprite;
+        SerializedProperty m_selectedSprite;
+
+
+        protected override void OnEnable()
+        {
+            m_useCheckBox = serializedObject.FindProperty("m_useCheckBox");
+            m_targetImage = serializedObject.FindProperty("targetImage");
+            m_normalSprite = serializedObject.FindProperty("normalSprite");
+            m_selectedSprite = serializedObject.FindProperty("selectedSprite");
+
+            base.OnEnable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            SrcToggle toggle = serializedObject.targetObject as SrcToggle;
+
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(m_useCheckBox, new GUIContent("Use Check Box"));
+            EditorGUILayout.PropertyField(m_targetImage, new GUIContent("Target Image"));
+            EditorGUILayout.PropertyField(m_normalSprite, new GUIContent("Normal Sprite"));
+            EditorGUILayout.PropertyField(m_selectedSprite, new GUIContent("Selected Sprite"));
+            if (EditorGUI.EndChangeCheck())
+            {
+                if (!Application.isPlaying)
+                    EditorSceneManager.MarkSceneDirty(toggle.gameObject.scene);
+
+                toggle.UseCheckBox = m_useCheckBox.boolValue;
+                toggle.targetImage = m_targetImage.objectReferenceValue as Image;
+                toggle.normalSprite = m_normalSprite.objectReferenceValue as Sprite;
+                toggle.selectedSprite = m_selectedSprite.objectReferenceValue as Sprite;
+            }
+
+            EditorGUILayout.Space();
+
+            base.OnInspectorGUI();
+        }
+    }
+}
---
+++ Assets/Source UI/SrcToggle.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using Unity.VisualScripting;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace UnityEngine.UI
+{
+    /// <summary>
+    /// A modded toggle to work with basic behaviour
+    /// </summary>
+    /// <remarks>
+    /// If you change or add a property that should be displayed in the inspector you have to handle that property in
+    /// <see cref="UnityEditor.UI.SrcToggleEditor"/>
+    /// </remarks>
+    [AddComponentMenu("UI/SrcToggle", 30)]
+    [RequireComponent(typeof(RectTransform))]
+    public class SrcToggle : Toggle
+    {
+        [SerializeField]
+        private bool m_useCheckBox = false;
+        private bool isCheckBoxOld = false;
+        public bool UseCheckBox {
+            get { return m_useCheckBox; }
+            set
+            {
+                m_useCheckBox = value;
+                HandleGraphics();
+            }
+        }
+
+        public Image targetImage;
+        public Sprite normalSprite;
+        public Sprite selectedSprite;
+
+
+        private void HandleGraphics()
+        {
+            if (transition != Transition.None)
+                transition = Transition.None;
+
+            if (isCheckBoxOld != UseCheckBox)
+            {
+                graphic.enabled = UseCheckBox ? true : false;
+                isCheckBoxOld = UseCheckBox;
+            }
+        }
+
+
+        private void InternalToggle()
+        {
+            Debug.Log("called InternalToggle!");
+
+            if (!IsActive() || !IsInteractable())
+                return;
+
+            isOn = !isOn;
+
+            var test = targetGraphic as Image;
+            targetImage.sprite = isOn ? selectedSprite : normalSprite;
+        }
+
+        /// <summary>
+        /// React to clicks.
+        /// </summary>
+        public override void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button != PointerEventData.InputButton.Left)
+                return;
+
+            InternalToggle();
+        }
+
+
+        protected override void OnValidate()
+        {
+            HandleGraphics();
+
+            base.OnValidate();
+        }
+    }
+}
